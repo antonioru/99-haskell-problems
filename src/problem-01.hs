6e4a01222db63ef5105ccbbe81fb0220c11c5ee9
Valeria Ceci <valeriaceci@mbp-di-valeria.home>
2020-01-18 22:43:39 +0000

    1st problem solution

--- src/problem-01.hs
+++ src/problem-01.hs
@@ -1,4 +1,6 @@
--- find the last element of our list
+-- Find the last element of a list.
 
-main = do
-  putStrLn "Hello, World!"
+-- Defining takeLast as a function that takes a list of generic type and returns an element of that type.
+takeLast :: [a] -> a
+takeLast [] = error "cannot take an element out of an empty list"
+takeLast list = list !! (-1 + length list) 
