dc584f1b0fbb294728c4b4f0f14008dd5af9d969
Antonio Russo <antonio@beautifulinteractions.com>
2020-01-18 22:49:35 +0000

    Alternative solution to the first problem

--- src/problem-01.hs
+++ src/problem-01.hs
@@ -4,3 +4,8 @@
 takeLast :: [a] -> a
 takeLast [] = error "cannot take an element out of an empty list"
 takeLast list = list !! (-1 + length list) 
+
+-- Alternative implementation by using head and reverse
+takeLast2 :: [a] -> a
+takeLast2 [] = error "Cannot tast element of an empty list"
+takeLast2 list = head (reverse list)
